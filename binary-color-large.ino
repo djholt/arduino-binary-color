c4f7679b75a0c6aeddbf9de02550db93fdfef2f5
DJ Holt <dj@holt.dj>
2021-05-28 10:46:57 -0600

    Initial commit

---
+++ binary-color-large.ino
@@ -0,0 +1,278 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include <CapacitiveSensor.h>
+
+#define JEWEL_PIN 28
+#define MATRIX_PIN 26
+#define RING_PIN 24
+#define SPEAKER_PIN 22
+
+#define JEWEL_SIZE 7
+#define MATRIX_SIZE 8
+#define RING_SIZE 60
+
+unsigned long debounceDelay = 50;
+unsigned long lastDebounceTimes[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool lastSensorStates[] = {false, false, false, false, false, false, false, false, false};
+bool sensorStates[] = {false, false, false, false, false, false, false, false, false};
+
+CapacitiveSensor s1 = CapacitiveSensor(3, 4);
+CapacitiveSensor s2 = CapacitiveSensor(3, 5);
+CapacitiveSensor s3 = CapacitiveSensor(3, 6);
+CapacitiveSensor s4 = CapacitiveSensor(3, 7);
+CapacitiveSensor s5 = CapacitiveSensor(3, 8);
+CapacitiveSensor s6 = CapacitiveSensor(3, 9);
+CapacitiveSensor s7 = CapacitiveSensor(3, 10);
+CapacitiveSensor s8 = CapacitiveSensor(3, 11);
+CapacitiveSensor s9 = CapacitiveSensor(3, 12);
+CapacitiveSensor *sensors[] = {&s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9};
+
+byte bytes[] = {0, 0, 0};
+int currentByteIndex = 0;
+int currentRingSpinPixel = 0;
+
+bool attractMode = true;
+long timeSinceActive = 0;
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(MATRIX_SIZE, MATRIX_SIZE, 5, 1, MATRIX_PIN,
+  NEO_TILE_TOP   + NEO_TILE_LEFT   + NEO_TILE_ROWS   + NEO_TILE_PROGRESSIVE +
+  NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel jewels = Adafruit_NeoPixel(JEWEL_SIZE * 8, JEWEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel rings = Adafruit_NeoPixel(RING_SIZE * 3, RING_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  initPixels();
+  displayCurrentByte();
+}
+
+void loop() {
+  checkSensors();
+  spinRing(currentByteIndex, currentByteColor32());
+}
+
+void checkSensors() {
+  if (checkSensorPressed(0)) {
+    fillRing(currentByteIndex, currentByteColor32());
+    for (int i = 0; i < 8; i++) {
+      resetJewel(i);
+    }
+    currentByteIndex++;
+    if (currentByteIndex == 3) {
+      toneColorSet();
+      displayCurrentColor();
+      outputBytes();
+      resetBytes();
+      for (int i = 0; i < 3; i++) {
+        resetRing(i);
+      }
+    } else {
+      toneByteSet();
+      displayCurrentByte();
+    }
+  }
+
+  for (int i = 0; i < 8; i++) {
+    if (checkSensorPressed(i+1)) {
+      if (toggleBit(i)) {
+        toneBitOn();
+        toggleJewelOn(8-i-1, currentByteColor32());
+      } else {
+        toneBitOff();
+        toggleJewelOff(8-i-1);
+      }
+      displayCurrentByte();
+    }
+  }
+}
+
+bool checkSensorPressed(int i) {
+  bool pressed = false;
+  long m = sensors[i]->capacitiveSensor(5);
+  bool reading = m > 100;
+
+  if (reading != lastSensorStates[i]) {
+    lastDebounceTimes[i] = millis();
+  }
+
+  if (millis() - lastDebounceTimes[i] > debounceDelay) {
+    if (reading != sensorStates[i]) {
+      sensorStates[i] = reading;
+
+      if (reading) {
+        pressed = true;
+        timeSinceActive = 0;
+      }
+    }
+  }
+
+  lastSensorStates[i] = reading;
+  return pressed;
+}
+
+uint16_t currentByteColor16() {
+  return matrix.Color(255 * (currentByteIndex == 0), 255 * (currentByteIndex == 1), 255 * (currentByteIndex == 2));
+}
+
+uint32_t currentByteColor32() {
+  return jewels.Color(255 * (currentByteIndex == 0), 255 * (currentByteIndex == 1), 255 * (currentByteIndex == 2));
+}
+
+void displayCurrentByte() {
+  matrix.fillScreen(0);
+  matrix.setTextColor(currentByteColor16());
+
+  String dec = String(bytes[currentByteIndex], DEC);
+
+  matrix.setCursor(20 - 3 * dec.length(), 0);
+  matrix.print(dec);
+
+  matrix.show();
+}
+
+void displayCurrentColor() {
+  uint16_t color = matrix.Color(bytes[0], bytes[1], bytes[2]);
+  uint16_t white = matrix.Color(255, 255, 255);
+
+  matrix.fillScreen(0);
+  matrix.setTextColor(color);
+
+  char hex[8];
+  sprintf(hex, "%02X%02X%02X", bytes[0], bytes[1], bytes[2]);
+
+  matrix.setCursor(5, 0);
+  matrix.print(hex);
+
+  for (int i = 0; i < 4; i++) {
+    matrix.drawPixel(i, 2, white);
+    matrix.drawPixel(i, 4, white);
+  }
+  for (int i = 0; i < 5; i++) {
+    matrix.drawPixel(1, i+1, white);
+    matrix.drawPixel(2, i+1, white);
+  }
+
+  matrix.show();
+}
+
+void initPixels() {
+  jewels.begin();
+  jewels.show();
+
+  matrix.begin();
+  matrix.setBrightness(50);
+  matrix.setTextWrap(false);
+  matrix.show();
+
+  rings.begin();
+  rings.show();
+}
+
+void outputBytes() {
+  Serial.println(String(bytes[0]) + ',' + String(bytes[1]) + ',' + String(bytes[2]));
+}
+
+void resetBytes() {
+  bytes[0] = 0;
+  bytes[1] = 0;
+  bytes[2] = 0;
+  currentByteIndex = 0;
+}
+
+bool toggleBit(int i) {
+  bytes[currentByteIndex] = bytes[currentByteIndex] ^ (1 << i);
+  return bytes[currentByteIndex] & (1 << i);
+}
+
+void toneBitOn() {
+  noTone(SPEAKER_PIN);
+  tone(SPEAKER_PIN, 3200, 100);
+  delay(110);
+}
+
+void toneBitOff() {
+  noTone(SPEAKER_PIN);
+  tone(SPEAKER_PIN, 2800, 100);
+  delay(110);
+}
+
+void toneByteSet() {
+  noTone(SPEAKER_PIN);
+  tone(SPEAKER_PIN, 600, 100);
+  delay(110);
+  tone(SPEAKER_PIN, 800, 100);
+  delay(110);
+  tone(SPEAKER_PIN, 1000, 200);
+  delay(220);
+}
+
+void toneColorSet() {
+  int i;
+  noTone(SPEAKER_PIN);
+  for (i = 0; i < 8; i++) {
+    tone(SPEAKER_PIN, 200 + 200 * i, 50);
+    delay(55);
+  }
+  noTone(SPEAKER_PIN);
+  for (i = 0; i < 8; i++) {
+    tone(SPEAKER_PIN, 400 + 200 * i, 50);
+    delay(55);
+  }
+  noTone(SPEAKER_PIN);
+  for (i = 0; i < 8; i++) {
+    tone(SPEAKER_PIN, 800 + 200 * i, 50);
+    delay(55);
+  }
+  tone(SPEAKER_PIN, 800 + 200 * i, 500);
+  delay(550);
+}
+
+void fillRing(int i, uint32_t color) {
+  resetRing(i);
+  for (int j = RING_SIZE * i; j < RING_SIZE * (i + 1); j++) {
+    rings.setPixelColor(j, color);
+    rings.show();
+    delay(3);
+  }
+}
+
+void resetJewel(int i) {
+  for (int j = JEWEL_SIZE * i; j < JEWEL_SIZE * (i + 1); j++) {
+    jewels.setPixelColor(j, 0);
+  }
+  jewels.show();
+}
+
+void resetRing(int i) {
+  for (int j = RING_SIZE * i; j < RING_SIZE * (i + 1); j++) {
+    rings.setPixelColor(j, 0);
+  }
+  rings.show();
+}
+
+void spinRing(int i, uint32_t color) {
+  resetRing(i);
+  rings.setPixelColor(RING_SIZE * i + currentRingSpinPixel, color);
+  currentRingSpinPixel = (currentRingSpinPixel + 1) % RING_SIZE;
+  rings.show();
+}
+
+void toggleJewelOn(int i, uint32_t color) {
+  for (int j = JEWEL_SIZE * i; j < JEWEL_SIZE * (i + 1); j++) {
+    jewels.setPixelColor(j, color);
+    jewels.show();
+    delay(30);
+  }
+}
+
+void toggleJewelOff(int i) {
+  for (int j = JEWEL_SIZE * (i + 1) - 1; j >= JEWEL_SIZE * i; j--) {
+    jewels.setPixelColor(j, 0);
+    jewels.show();
+    delay(30);
+  }
+}
+
